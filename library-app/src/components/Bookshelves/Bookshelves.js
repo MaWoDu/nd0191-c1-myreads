e47b985d42b31803d59b77de66a6bd6ff9291dfc
mawodu <durkiewiczmaciej@gmail.com>
2024-09-24 20:44:04 +0100

    refactor(bookshelf): extract bookshelf component

--- library-app/src/components/Bookshelves/Bookshelves.js
+++ library-app/src/components/Bookshelves/Bookshelves.js
@@ -45,24 +45,32 @@ const books = [
     },
 ]
 
+function Bookshelf({category, books}) {
+    return <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+                {books.map(book => {
+                        return (<Book
+                            title={book.title}
+                            authors={book.authors}
+                            coverUrl={book.coverUrl}
+                        />)
+                    }
+                )}
+            </ol>
+        </div>
+    </div>;
+}
+
 export const Bookshelves = () => {
     return (
         <div className="list-books-content">
             <div>
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {books.map(book => {
-                                return (<Book
-                                    title={book.title}
-                                    authors={book.authors}
-                                    coverUrl={book.coverUrl}
-                                />)
-                            })}
-                        </ol>
-                    </div>
-                </div>
+                <Bookshelf
+                    category={"Currently Reading"}
+                    books={books}
+                />
             </div>
         </div>
     )
