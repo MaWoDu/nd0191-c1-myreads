702799f220cf0aff05d68ebd2788d508c236032b
mawodu <durkiewiczmaciej@gmail.com>
2024-09-24 21:10:20 +0100

    feat(categories): extend categories to provide slug and visibility flag

--- library-app/src/components/Bookshelves/Categories.js
+++ library-app/src/components/Bookshelves/Categories.js
@@ -1,1 +1,37 @@
-export const categories = ["Currently Reading", "Want to Read", "Read", "None"]
+export const categories = [
+    {
+        label: "Currently Reading",
+        slug: toSlug(this.label),
+        isVisible: true
+    },
+    {
+        label: "Want to Read",
+        slug: toSlug(this.label),
+        isVisible: true
+    },
+    {
+        label: "Read",
+        slug: toSlug(this.label),
+        isVisible: true
+    },
+    {
+        label: "None",
+        slug: toSlug(this.label),
+        isVisible: false
+    }
+]
+
+function toSlug(str) {
+    return str
+        .replace(/[^a-zA-Z0-9 ]/g, '')
+        .trim()
+        .split(' ')
+        .map((word, index) => {
+            if (index === 0) {
+                return word.toLowerCase();
+            } else {
+                return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+            }
+        })
+        .join('');
+}
