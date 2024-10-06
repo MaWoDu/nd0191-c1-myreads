0b6a01ab8cfdb7e4da480d64de662165baa6bb1f
mawodu <durkiewiczmaciej@gmail.com>
2024-10-06 22:23:17 +0100

    refactor(debounce): inline debounce function

--- library-app/src/pages/GlobalLibrary.js
+++ library-app/src/pages/GlobalLibrary.js
@@ -15,29 +15,32 @@ export const GlobalLibrary = ({linkToHome}) => {
         apiSearch(updatedSearchTerm)
     }
 
-    // todo: handle warning
     const apiSearch = useCallback(
-        debounce(async (searchTerm) => {
-            search(searchTerm, 1) // fixme: maxResults value is ignored
-                .then(result => {
-                    if (Array.isArray(result)) {
-                        setBooks(result);
-                    } else if (result && result.error) {
-                        console.error('Error response received:', result.error);
-                        setBooks([])
-                    } else {
-                        console.error('Unexpected response format:', result);
-                        setBooks([])
-                    }
-                })
-                .catch(error => setBooks([]))
-        }, 300),
-        [debounce]
+        (searchTerm) => {
+            const localDebounce = debounce(async (searchTerm) => {
+                search(searchTerm, 1) // fixme: maxResults value is ignored
+                    .then((result) => {
+                        if (Array.isArray(result)) {
+                            setBooks(result);
+                        } else if (result && result.error) {
+                            console.error('Error response received:', result.error);
+                            setBooks([]);
+                        } else {
+                            console.error('Unexpected response format:', result);
+                            setBooks([]);
+                        }
+                    })
+                    .catch((error) => setBooks([]));
+            }, 300);
+
+            return localDebounce(searchTerm);
+        },
+        []
     );
 
     return (<div className="search-books">
         <Header breadcrumb={"Global Library"}/>
         <LibrarySearch linkToHome={linkToHome} searchTerm={searchTerm} search={searchCallback}/>
-        {  books && books.length > 0 ? <Library books={books} setBooksCallback={setBooks}/> : <></> }
+        {books && books.length > 0 ? <Library books={books} setBooksCallback={setBooks}/> : <></>}
     </div>)
 }
